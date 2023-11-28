f4e49c5f11e3373b1975c371ea7438fa859a3d76
tuvalp <tuvalpeled@gmail.com>
2023-11-28 11:45:44 +0200

    a

--- lib/main.dart
+++ lib/main.dart
@@ -2,7 +2,6 @@ import 'package:flutter/material.dart';
 import 'package:flutter/services.dart';
 
 import 'package:hive_flutter/hive_flutter.dart';
-import 'package:permission_handler/permission_handler.dart';
 
 import 'package:get/get.dart';
 
