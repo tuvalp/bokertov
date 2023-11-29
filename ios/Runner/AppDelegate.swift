7659fbaf12fb7e65828342f34527ac63bb00f64f
tuvalp <tuvalpeled@gmail.com>
2023-11-29 10:18:39 +0200

    2

--- ios/Runner/AppDelegate.swift
+++ ios/Runner/AppDelegate.swift
@@ -8,6 +8,8 @@ import Flutter
     var methodChannel: FlutterMethodChannel?
 
     override func application(_ application: UIApplication, didFinishLaunchingWithOptions launchOptions: [UIApplication.LaunchOptionsKey: Any]?) -> Bool {
+        GeneratedPluginRegistrant.register(with: self)
+        
         let controller: FlutterViewController = window?.rootViewController as! FlutterViewController
         methodChannel = FlutterMethodChannel(name: "com.example.bokertov", binaryMessenger: controller.binaryMessenger)
         print("IOS onCreate!!!")
