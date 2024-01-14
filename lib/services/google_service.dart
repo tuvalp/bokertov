276f537a2ed80dbfea1ef3228fd21e6244a317c0
tuvalp <tuvalpeled@gmail.com>
2024-01-14 12:03:56 +0200

    google fix

--- lib/services/google_service.dart
+++ lib/services/google_service.dart
@@ -1,8 +1,5 @@
 import 'package:flutter/foundation.dart';
 import 'package:google_sign_in/google_sign_in.dart';
-import 'package:googleapis/fitness/v1.dart' as fitness;
-import 'package:googleapis_auth/googleapis_auth.dart' as auth;
-import 'package:http/http.dart' as http;
 import 'package:hive_flutter/hive_flutter.dart';
 import '../services/account_box_item.dart';
 
@@ -53,34 +50,5 @@ class GoogleService {
 
   void singOption() {
     _googleSignIn!.signIn();
-  }
-
-  Future<void> fetchData() async {
-    try {
-      if (_currentUser == null) {
-        print('User not signed in.');
-        return;
-      }
-
-      GoogleSignInAccount account = _googleSignIn!.currentUser!;
-      GoogleSignInAuthentication _auth = await account.authentication;
-      final client = await auth.clientViaUserConsent(
-        ClientId(
-          'YOUR_CLIENT_ID',
-          'YOUR_CLIENT_SECRET',
-        ),
-        scopes,
-        promptUser: true,
-      );
-
-      final fitnessApi = fitness.FitnessApi(client);
-
-      // Use fitnessApi to make requests and fetch data
-      // Example: List the user's data sources
-      var dataSources = await fitnessApi.users.dataSources.list('me');
-      print(dataSources);
-    } catch (error) {
-      print('Fetch Data Error: $error');
-    }
   }
 }
